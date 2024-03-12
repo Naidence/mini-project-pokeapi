e13dcc07f9d25841c243b44e9bce9be35a1521cc
Naidence <nadhif.razan@gmail.com>
2024-03-12 22:56:16 +0700

    update frontend

--- file.py
+++ file.py
@@ -90,7 +90,7 @@ def search():
 
         return render_template('result.html', pokemon=result)
     else:
-        return render_template('error.html')
+        return "Nama pokemonmu salah"
 
 
 # RATINGS
@@ -107,6 +107,69 @@ def rate_pokemon():
         return f"Rated {pokemon_name} with a score of {rating}"
     else:
         return "Please provide both a Pokémon name and a rating."
+
+# def post(cls, name):
+#         try:
+#             rules = {
+#                 'star': ['required', 'numeric'], 
+#                 'title': ['required', 'string'],
+#                 'content': ['required', 'string']
+#             }
+#             # messages = {
+#             #     'name.required': 'Yikes! The name is required',
+#             #     'name.alpha': 'Yikes! The name must be a string',
+#             # }
+            
+#             validator = Validator(rules=rules, request=request)
+#             if validator.passes():
+#                 user = PokeReview(
+#                     id = uuid.uuid4(),
+#                     star=request.json['star'],
+#                     title=request.json['title'],
+#                     content=request.json['content'],
+#                     pokemon_name = name,
+#                     user_ip=request.remote_addr,
+#                     user_agent=request.user_agent.string
+#                 )
+#                 db.session.add(user)
+#                 db.session.commit()
+
+#                 return make_response(jsonify({'message': 'success'}), 200)
+#             else:
+#                 return make_response(jsonify(validator.messages()), 400)
+#         except Exception as e:  
+#             current_app.logger.error(traceback.format_exc())
+#             return make_response(jsonify({'error': 'Internal Server Error'}), 500)
+        
+# def get(cls, name):
+#         try:
+#             rules = {
+#                 'name': ['required', 'alpha']
+#             }
+#             messages = {
+#                 'name.required': 'Yikes! The name is required',
+#                 'name.alpha': 'Yikes! The name must be a string',
+#             }
+            
+#             validator = Validator(rules=rules, messages=messages, request={'name': name})
+#             if validator.passes():
+#                 reviews = PokeReview.query.filter_by(pokemon_name=name).all()
+#                 return make_response(jsonify([{
+#                     'id': str(review.id),
+#                     'star': review.star,
+#                     'title': review.title,
+#                     'content': review.content,
+#                     'pokemon_name': review.pokemon_name,
+#                     'user_ip': review.user_ip,
+#                     'user_agent': review.user_agent,
+#                     'created_at': review.created_at,
+#                     'updated_at': review.updated_at
+#                 } for review in reviews]), 200)
+#             else:
+#                 return make_response(jsonify(validator.messages()), 400)
+#         except Exception as e:  
+#             current_app.logger.error(traceback.format_exc())
+#             return make_response(jsonify({'error': 'Internal Server Error'}), 500)
 
 
 def get_pokemon_details(pokemon_url):
